77c780b9a52c431a590235c0a8438b4e5e401eff
copanicus <udemenick@gmail.com>
2022-12-20 13:39:33 -0800

    Task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define the Square."""
+
+
+class Square:
+    """Representing a Square."""
+    def __init__(self, size):
+        """Initializing a square
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
