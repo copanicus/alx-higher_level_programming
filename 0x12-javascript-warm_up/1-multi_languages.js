3b5f8af5c7682dafce9f3c4cd0d178ef07fe3b80
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2023-03-13 20:12:11 -0400

    All Task

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-// A script that prints 3 lines about C, Python and JS
+// a script that prints 3 lines
 
-const myVar1 = 'C is fun';
-const myVar2 = 'Python is cool';
-const myVar3 = 'Javascript is amazing';
+// created constant variables, each with the desired message.
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
 
-console.log(myVar1 + '\n' + myVar2 + '\n' + myVar3);
+// the console.log() function is used three times to print each line to the console, one at a time.
+console.log(line1);
+console.log(line2);
+console.log(line3);
