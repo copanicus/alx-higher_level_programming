18d6f78361ef05b82b078458b5008fd3c5dd3324
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2023-03-18 16:07:31 -0400

    Task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
-import MySQLdb
+"""script that lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db_connection = MySQLdb.connect(host='localhost',
+                                    port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3])
+    cur = db_connection.cursor()
+    cur.execute("""SELECT * FROM states
+            WHERE name LIKE BINARY '{}'
+            ORDER BY id ASC""".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
