69ee572fa16896cffe48d6b720204b594485fe4b
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2023-04-07 10:03:14 -0400

    Task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-""" Python script that takes your GitHub credentials (username and password)
-and uses the GitHub API to display your id """
-import requests
+"""A script that:
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
+"""
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/user'
-    username = sys.argv[1]
-    token = sys.argv[2]
-
-    response = requests.get(url, auth=(username, token))
-    try:
-        print(response.json().get('id'))
-        #print(response.json())
-    except ValueError:
-        print('Not a valid JSON')
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
