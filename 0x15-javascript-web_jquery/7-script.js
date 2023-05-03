4287efac43cd586f3dfe930cc080eff3d261e839
Iniobong Udeme <63008538+copanicus@users.noreply.github.com>
2023-05-03 17:09:46 -0400

    Task7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 $(document).ready(function () {
   $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data, textStatus) {
     if (textStatus === 'success') {
